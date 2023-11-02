ee7da7cc69eee22b75c0464a2e9f5d7ca759e84c
Anlin Babu <anlin@qburst.com>
2023-11-02 15:07:07 +0530

    setup routs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,30 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import { store } from "./store/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
+import LandingPage from "./Views/LandingPage";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    children: [
+      // {
+      //   path: "team",
+      //   element: <Team />,
+      //   loader: teamLoader,
+      // },
+    ],
+  },
+]);
 function App() {
   const [count, setCount] = useState(0);
-  return <Provider store={store}></Provider>;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 }
 
 export default App;
