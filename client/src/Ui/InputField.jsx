36aac1fc415158e7c48564aa60afcdb25474963c
Anlin Babu <anlin@qburst.com>
2023-11-03 10:04:35 +0530

    Created the reusable input field

---
+++ client/src/Ui/InputField.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classes from "./InputField.module.css";
+import SearchIcon from "../assets/icons/SearchIcon";
+import ArrowDown from "../assets/icons/ArrowDown";
+import Warning from "../assets/icons/Warning";
+InputField.propTypes = {
+  setValue: PropTypes.func,
+  status: PropTypes.oneOf(["active", "inactive", "error", "success"]),
+  type: PropTypes.oneOf(["search", "dropdown", "dropdownsearch", ""]),
+};
+export default function InputField({ setValue, status, type }) {
+  return (
+    <div className={classes.inputwrapper}>
+      <label>First</label>
+      <div className={`${classes.inputcontainer} ${classes[status]}`}>
+        {(type === "search" || type === "dropdownsearch") && (
+          <div className={classes.icon}>
+            <SearchIcon height={18} width={18} />
+          </div>
+        )}
+
+        <input
+          type="text"
+          className={`${
+            type === "search" && type === "dropdownsearch"
+              ? classes.inputnopadding
+              : classes.inputaddpadding
+          }`}
+          placeholder={"Input"}
+        />
+        {(type === "search" || type === "dropdown") && status !== "error" && (
+          <div className={classes.icon}>
+            <ArrowDown height={18} width={18} />
+          </div>
+        )}
+        {status === "error" && (
+          <div className={classes.icon}>
+            <Warning />
+          </div>
+        )}
+      </div>
+      <text>Supporting text</text>
+    </div>
+  );
+}
--- client/src/Views/LandingPage.jsx
+++ client/src/Views/LandingPage.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import InputField from "../Ui/InputField";
+import classes from "./LandingPage.module.css";
 export default function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <div className={classes.test}>
+      <InputField status={"aofapfk"} />
+    </div>
+  );
 }
